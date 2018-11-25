a523132d3e684e9dfa971ca2c55771002d4f458d
ytksy1218 <ytksy1218@gmail.com>
2018-11-25 13:37:46 +0900

    refactor camera rotation

--- Assets/Scripts/PointOfViewController.cs
+++ Assets/Scripts/PointOfViewController.cs
@@ -2,11 +2,16 @@
 
 public class PointOfViewController : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+    private Vector3 _rotate;
+
     void Update()
     {
-        transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
-        transform.Rotate(Input.GetAxis("Mouse Y"), 0, 0);
-        transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, 0.0f);
+//        transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+//        transform.Rotate(Input.GetAxis("Mouse Y"), 0, 0);
+//        transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, 0.0f);
+
+        _rotate += new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * _speed;
+        transform.rotation = Quaternion.Euler(_rotate);
     }
 }
-
